a440dd10fd0587cb886644f17ecb3b385b985748
ReiKoka <reikoka1@outlook.com>
2025-02-25 17:58:27 +0100

    Changed the theme colors

--- src/components/Landing/LandingLinks.tsx
+++ src/components/Landing/LandingLinks.tsx
@@ -1,9 +1,11 @@
 import { Link } from "@tanstack/react-router";
+import Button from "../ui/Button";
 
 function LandingLinks() {
   return (
     <nav className="font-secondary">
-      <Link to="/login">Login</Link>
+      <Link  to="/login">Login</Link>
+      <Button>Rei</Button>
     </nav>
   );
 }
