95fe7a7742e037ff13431a834b87b53ac0c3c8de
ReiKoka <reikoka1@outlook.com>
2025-02-27 18:07:11 +0100

    Old way with state for investigating register weird behaviour

--- src/routes/register.tsx
+++ src/routes/register.tsx
@@ -1,7 +1,7 @@
 import { createFileRoute, Link } from "@tanstack/react-router";
 import Input from "../components/ui/Input";
 import { useState } from "react";
-import { FormRegisterType, RoleType } from "../utils/types";
+import { FormRegisterType } from "../utils/types";
 import RegisterSVG from "../assets/images/register.svg?react";
 import { HiEnvelope, HiLockClosed, HiPhoto } from "react-icons/hi2";
 import Button from "../components/ui/Button";
@@ -13,22 +13,32 @@ export const Route = createFileRoute("/register")({
   component: RouteComponent,
 });
 
-const submitData = async (userData: FormData) => {
-  const id = nanoid(10);
-  const newUser = {
-    id,
-    firstName: userData.get("firstName") as string,
-    lastName: userData.get("lastName") as string,
-    email: userData.get("email") as string,
-    password: userData.get("password") as string,
-    avatar: userData.get("avatar") as string,
-    role: "admin" as RoleType,
-  };
-  console.log(newUser);
-  await register(newUser);
-};
-
 function RouteComponent() {
+  const [formData, setFormData] = useState<FormRegisterType>({
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+    avatar: "",
+    role: "admin",
+  });
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { id, value } = e.target;
+    setFormData((prevData) => ({ ...prevData, [id]: value }));
+  };  
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const id = nanoid(10);
+
+    const newUser = { ...formData, id };
+    console.log(newUser);
+    register(newUser);
+
+  };
+
   return (
     <div className="h-full w-full lg:grid lg:grid-cols-2">
       <div className="hidden h-full items-center justify-center lg:flex">
@@ -36,7 +46,7 @@ function RouteComponent() {
       </div>
       <div className="flex h-full items-center justify-center">
         <form
-          action={submitData}
+          onSubmit={handleSubmit}
           className="bg-background flex w-8/12 max-w-[600px] min-w-[280px] flex-col gap-6 rounded-lg p-4 sm:p-6 md:p-8"
         >
           <h2 className="font-secondary mb-10 text-center text-xl font-semibold">
@@ -48,12 +58,16 @@ function RouteComponent() {
               id="firstName"
               type="text"
               label="First Name"
+              value={formData.firstName}
+              onChange={handleChange}
               icon={<HiUser />}
             />
             <Input
               id="lastName"
               type="text"
               label="Last Name"
+              value={formData.lastName}
+              onChange={handleChange}
               icon={<HiUser />}
             />
           </div>
@@ -63,12 +77,16 @@ function RouteComponent() {
               id="email"
               type="email"
               label="Email"
+              value={formData.email}
+              onChange={handleChange}
               icon={<HiEnvelope />}
             />
             <Input
               id="password"
               type="password"
               label="Password"
+              value={formData.password}
+              onChange={handleChange}
               icon={<HiLockClosed />}
             />
           </div>
@@ -77,6 +95,8 @@ function RouteComponent() {
             id="avatar"
             type="text"
             label="Profile Picture"
+            value={formData.avatar || ""}
+            onChange={handleChange}
             icon={<HiPhoto />}
           />
 
