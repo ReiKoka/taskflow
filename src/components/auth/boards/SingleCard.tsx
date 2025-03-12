7eeea39702ae912f540de5113ecceb4c3e18aac4
ReiKoka <reikoka1@outlook.com>
2025-03-12 14:04:33 +0100

    Added some design changes for the checkbox and the paragraph when the status is completed for better ux

--- src/components/auth/boards/SingleCard.tsx
+++ src/components/auth/boards/SingleCard.tsx
@@ -25,14 +25,14 @@ function SingleCard({ item }: SingleCardProps) {
   return (
     <div className="flex flex-col gap-2">
       <div
-        className="group bg-muted flex w-full cursor-pointer items-center gap-4 overflow-hidden rounded-lg px-3 py-2"
+        className="group bg-muted flex w-full cursor-pointer items-center gap-2 overflow-hidden rounded-lg px-2 py-2"
         draggable
         onDragStart={handleDragStart}
       >
         <div
           tabIndex={1}
           onClick={handleStatusChange}
-          className={`ring-offset-muted border-border flex aspect-square h-4 w-4 -translate-x-4 cursor-pointer items-center justify-center rounded-full border opacity-0 ring-green-400 ring-offset-2 transition-all duration-300 ease-in group-hover:translate-x-0 group-hover:opacity-100 group-hover:ease-out focus-visible:ring focus-visible:outline-0 ${status === "completed" && "border-green-500 bg-green-500 dark:border-green-700 dark:bg-green-700 dark:ring-green-700"}`}
+          className={`ring-offset-muted border-border flex aspect-square h-4 w-4 cursor-pointer items-center justify-center rounded-full border opacity-0 ring-green-400 ring-offset-2 transition-all duration-300 ease-in group-hover:ease-out focus-visible:ring focus-visible:outline-0 ${status === "completed" ? "border-green-500 bg-green-500 opacity-100 dark:border-green-700 dark:bg-green-700 dark:ring-green-700" : "-translate-x-4 group-hover:translate-x-0 group-hover:opacity-100"}`}
         >
           <HiCheck
             size={10}
@@ -44,7 +44,9 @@ function SingleCard({ item }: SingleCardProps) {
             }`}
           />
         </div>
-        <p className="max-w-full -translate-x-4 overflow-hidden text-xs font-medium break-words text-ellipsis whitespace-normal transition-transform duration-300 ease-in group-hover:translate-x-0 group-hover:ease-out">
+        <p
+          className={`max-w-full overflow-hidden text-xs font-medium break-words text-ellipsis whitespace-normal transition-transform duration-300 ease-in group-hover:translate-x-0 group-hover:ease-out ${status === "completed" ? "translate-x-0" : "-translate-x-4"}`}
+        >
           {item?.title}
         </p>
       </div>
